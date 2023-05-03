1314b339438539e5f8a0dbe76698454d3e079733
Vishal Gautam <ivishalgautamm@gmail.com>
2023-05-03 18:42:07 +0530

    api key changed

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,4 +1,4 @@
-=import { useParams, Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
